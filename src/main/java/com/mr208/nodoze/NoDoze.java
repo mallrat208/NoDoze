e0f92ad65a64b0c1fcb3090dab2d2be6f9af5bad
Andrew W <marwat208@gmail.com>
2017-11-20 04:12:16 -0500

    Updated to 1.12.2
    Updated to 1.12.2

--- src/main/java/com/mr208/nodoze/NoDoze.java
+++ src/main/java/com/mr208/nodoze/NoDoze.java
@@ -4,55 +4,53 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Biomes;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
-
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.event.entity.player.PlayerSleepInBedEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = NoDoze.MODID, name = NoDoze.MODNAME, version = NoDoze.MODVERSION)
+@Mod(modid = NoDoze.MOD_ID, name = NoDoze.MOD_NAME, version = NoDoze.MOD_VER)
 public class NoDoze {
 
-	public static final String MODID = "nodoze";
-	public static final String MODNAME = "No Doze";
-	public static final String MODVERSION = "1.0.0";
-
-	public static Configuration config;
-	public static boolean setSpawnDay;
-
-	@Mod.Instance(MODID)
-	public static NoDoze instance = new NoDoze();
+	public static final String MOD_ID = "nodoze";
+	public static final String MOD_NAME = "No Doze";
+	public static final String MOD_VER = "1.0.0";
 
 	@Mod.EventHandler
-	public void preInit(FMLPreInitializationEvent event)
-	{
-		MinecraftForge.EVENT_BUS.register(this);
+	public void onInit(FMLInitializationEvent event) {
 
-		config = new Configuration(event.getSuggestedConfigurationFile());
-		config.load();
-		setSpawnDay = config.get("Settings","SetSpawnAnyTime",true).getBoolean();
-		config.save();
-
+		MinecraftForge.EVENT_BUS.register(SleepEvents.INSTANCE);
 	}
 
-	@SubscribeEvent
-	public void onPlayerSleep(PlayerSleepInBedEvent event)
-	{
-		event.setResult(EntityPlayer.SleepResult.OTHER_PROBLEM);
+	@net.minecraftforge.common.config.Config(modid = MOD_ID)
+	public static class Config {
 
-		World world = event.getEntityPlayer().getEntityWorld();
+		@net.minecraftforge.common.config.Config.Comment({"If true, players can set their spawn at any point during the day"})
+		public static boolean setSpawnDay = true;
+	}
 
-		if(world.isDaytime() && !setSpawnDay) return;
+	public static class SleepEvents {
 
-		if(world.provider.canRespawnHere() && world.provider.getBiomeForCoords(event.getPos()) != Biomes.HELL)
-		{
+		public static SleepEvents INSTANCE = new SleepEvents();
+
+		@SubscribeEvent
+		public void onPlayerSleep(PlayerSleepInBedEvent event) {
 			EntityPlayer entityPlayer = event.getEntityPlayer();
-			entityPlayer.setSpawnPoint(event.getPos(), false);
-			entityPlayer.setSpawnChunk(event.getPos(), false, event.getEntityPlayer().dimension);
-			entityPlayer.sendStatusMessage(new TextComponentTranslation("nodoze.spawn.set"),true);
-		}
+			World world = event.getEntityPlayer().getEntityWorld();
 
+			event.setResult(EntityPlayer.SleepResult.OTHER_PROBLEM);
+			entityPlayer.sendStatusMessage(new TextComponentTranslation("nodoze.sleep.denied"), true);
+
+			if(world.isDaytime() && !Config.setSpawnDay) return;
+
+			if(world.provider.canRespawnHere() && (world.provider.getBiomeForCoords(event.getPos()) != Biomes.HELL)) {
+				entityPlayer.setSpawnPoint(event.getPos(), false);
+				entityPlayer.setSpawnChunk(event.getPos(), false, event.getEntityPlayer().dimension);
+
+				if(world.isDaytime())
+					entityPlayer.sendStatusMessage(new TextComponentTranslation("nodoze.spawn.set"),true);
+			}
+		}
 	}
 }
